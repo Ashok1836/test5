59b7c2024639d9998d98b5bda7c32326b4f2404d
Ashok Reddy <ashokreddy@ls-phy-lap-0113.local>
2019-01-16 14:32:01 +0530

    test5Demo is the new project

---
+++ Test5Demo/Test5Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test5Demo
+//
+//  Created by Ashok Reddy on 16/01/19.
+//  Copyright © 2019 Ashok Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
